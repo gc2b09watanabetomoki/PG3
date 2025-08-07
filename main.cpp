36638f38ed4172d03321b289c733a156c3f632e8
WT <k024g1006@g.neec.ac.jp>
2025-08-07 14:35:41 +0900

    邪魔なコメントを一部削除

--- main.cpp
+++ main.cpp
@@ -4,28 +4,13 @@
 #include <iostream>
 
 int main() {
-	/*
-	// 左辺値   右辺値
-	// int i  =  1;
-	
-	// 右辺値参照型
-	// int&& c = 1; // 左辺値と同じだけ生き残る
-
-	// 右辺値から左辺値へのキャスト
-	// std::move は左辺値を右辺値にキャストする
-	//int c;
-	//std::move(c);
-
-	// 右辺値が扱えると何が嬉しいのか？
-	//  コピーではなくムーブができる
-	*/
 	
 	std::string a(1000000, 'a');
 	std::string b(1000000, 'b');
 	std::string c(1000000, 'c');
 	
 	// 文字をコピーして時間を計測
-	 // 計測開始
+	// 計測開始
 	auto copyStart = std::chrono::system_clock::now();
 	
 	b = a;
@@ -35,7 +20,7 @@ int main() {
 	auto copyTime = std::chrono::duration_cast<std::chrono::microseconds>(copyEnd - copyStart);
 
 	// 文字を移動して時間を計測
-	 // 計測開始
+	// 計測開始
 	auto moveStart = std::chrono::system_clock::now();
 
 	c = std::move(a);
